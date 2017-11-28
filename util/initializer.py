9841281ba8ba61979f2164ca098e7ca0d8be063e
Daniel Yoo <comadan@gmail.com>
2017-11-27 18:06:03 -0800

    add data types.

--- util/initializer.py
+++ util/initializer.py
@@ -1,23 +1,25 @@
-def uniform_random_weights(k_out, k_in, scale):
+import numpy as np
+
+def uniform_random_weights(k_out, k_in, scale, dtype=np.float32):
     """
     Returns weights of shape (k_in, k_out) initialized between [-scale, scale]
     """
-    return ((np.random.rand(k_in, k_out) * 2 - 1) * scale)
+    return ((np.random.rand(k_in, k_out) * 2 - 1) * scale).astype(dtype)
 
-def random_weights_tanh(k_in, k_out):
+def random_weights_tanh(k_out, k_in, dtype=np.float32):
     scale = (6. / (k_in + k_out)) ** .5
-    return uniform_random_weights(k_out, k_in, scale)
+    return uniform_random_weights(k_out, k_in, scale, dtype=dtype)
 
-def random_weights_sigmoid(k_out, k_in):
+def random_weights_sigmoid(k_out, k_in, dtype=np.float32):
     scale = 4. * (6. / (k_in + k_out)) ** .5
-    return uniform_random_weights(k_out, k_in, scale)
+    return uniform_random_weights(k_out, k_in, scale, dtype=dtype)
 
-def random_weights_reLu(k_out, k_in):
+def random_weights_reLu(k_out, k_in, dtype=np.float32):
     scale = (2. / (k_in + k_out)) ** .5
-    return uniform_random_weights(k_out, k_in, scale)
+    return uniform_random_weights(k_out, k_in, scale, dtype=dtype)
 
-def biases(k):
+def biases(k, dtype=np.float32):
     """
     Initialize biases as zero.
     """
-    return np.zeros((k, ))
+    return np.zeros((k, ), dtype=dtype)
